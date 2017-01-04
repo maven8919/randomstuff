aa5659b00bd0cc85c971e8d71dd7b95b4473e9b7
Levente Bakos <levente_bakos@epam.com>
2017-01-04 16:34:43 +0100

    Initial commit

---
+++ src/main/kotlin/com/maven8919/RandomstuffApplication.kt
@@ -0,0 +1,11 @@
+package com.maven8919
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+open class RandomstuffApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(RandomstuffApplication::class.java, *args)
+}
